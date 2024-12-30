85434e7980f0de00ee12d4cca2bdfb57301b8051
rosemwende <rosemuema82@gmail.com>
2024-12-30 05:55:02 +0000

    css_parser.py

--- sass_scss/css_parser.py
+++ sass_scss/css_parser.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import os
 import re
 
 class CSSParser():
@@ -11,67 +10,64 @@ class CSSParser():
         if self.file_path is None:
             return False
         
-        file_content = None
         with open(self.file_path, "r") as file:
             file_content = file.read()
-        if file_content is None:
+
+        if not file_content:
             return False
 
-        # cleanup
+        # Cleanup
         file_content = self.__remove_comments(file_content)
         file_content = self.__remove_newline(file_content)
         
-        # parsing
+        # Parsing
         self.__parse(file_content)
         return self.selectors
 
     def __remove_comments(self, s):
-        s = re.sub(re.compile("/\*.*?\*/", re.DOTALL), "" , s) 
-        s = re.sub(re.compile("//.*?\n"), "" , s)
-        return s
-    
-    def __remove_newline(self, s):
-        s = re.sub(re.compile("\n"), "" , s)
-        return s
+        return re.sub(r"/\*.*?\*/", "", s, flags=re.DOTALL)
 
-    def __remove_spaces(self, s):
-        s = re.sub(re.compile(" "), "" , s)
-        return s
+    def __remove_newline(self, s):
+        return s.replace("\n", "")
+
+    def __remove_extra_spaces(self, s):
+        return re.sub(r"\s+", " ", s).strip()
 
     def __clean_key(self, s):
-        return s.lstrip().rstrip()
+        return s.strip()
 
     def __parse(self, s):
         self.selectors = {}
-        split_medias_query = s.split("@media")
-        if len(split_medias_query) == 1:
-            self.__parse_in_media_query(split_medias_query[0])
-        else:
-            for medias_query in split_medias_query:
-                in_media_query = None
-                if "screen and" in medias_query:
-                    in_media_query = medias_query.split("{")[0].split("max-width: ")[1].split(")")[0]
-                    medias_query = "}".join("{".join(medias_query.split("{")[1:]).split("}")[:-1])
-                self.__parse_in_media_query(medias_query, in_media_query)
+        for block in s.split("}"):
+            if "{" in block:
+                selector, properties = block.split("{")
+                selector = self.__clean_key(selector)
+                properties = self.__parse_properties(properties)
+                self.selectors[selector] = properties
 
-    def __parse_in_media_query(self, s, media_query=None):
-        split_lefts = s.split("}")
+    def __parse_properties(self, properties_str):
+        properties = {}
+        for prop in properties_str.split(";"):
+            if ":" in prop:
+                name, value = prop.split(":", 1)
+                properties[self.__clean_key(name)] = self.__remove_extra_spaces(value)
+        return properties
 
-        for split_left in split_lefts:
-            split_rights = split_left.split("{")
-            if len(split_rights) != 2:
-                continue
-            keys_selector = self.__clean_key(split_rights[0]).split(",")
-            
-            split_commas = split_rights[1].split(";")
-            for split_comma in split_commas:
-                split_styles = split_comma.split(":")
-                if len(split_styles) != 2:
-                    continue
-                for key_selector in keys_selector:
-                    key_selector = self.__clean_key(key_selector)
-                    if media_query is not None:
-                        key_selector = "{}__{}".format(key_selector, media_query)
-                    if self.selectors.get(key_selector) is None:
-                        self.selectors[key_selector] = {}
-                    self.selectors[key_selector][self.__clean_key(split_styles[0])] = self.__remove_spaces(split_styles[1])
+    def validate_property(self, selector, property_name, expected_value):
+        selector_data = self.selectors.get(selector)
+        if not selector_data:
+            return False
+        return selector_data.get(property_name) == expected_value
+
+
+if __name__ == "__main__":
+    css_parser = CSSParser("test.css")
+    selectors = css_parser.parse()
+
+    print("Selectors Parsed:", selectors)
+
+    button_valid = css_parser.validate_property("button", "color", "#FF0000")
+    hover_valid = css_parser.validate_property("button:hover", "color", "#00FF00")
+
+    print("Button color valid:", button_valid)
+    print("Button:hover color valid:", hover_valid)
